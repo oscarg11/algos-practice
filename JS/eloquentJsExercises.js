4a4481f9575a62f93e89c13b8a3e1d030ebb989c
oscarg11 <oscar.g2511@gmail.com>
2023-11-23 20:16:10 -0500

    Solved looping a triangle exercise

---
+++ JS/eloquentJsExercises.js
@@ -0,0 +1,19 @@
+// 1) Looping a Triangle
+/****************
+Write a loop that makes seven calls to console.log
+to output the following triangle..
+#
+##
+###
+####
+#####
+######
+#######
+ ****************/
+console.log("1.) Looping A Trianlge")
+
+for(let triangle ="#"; triangle.length < 7; triangle+="#" ){
+    console.log(triangle);
+}
+
+console.log("--------")
