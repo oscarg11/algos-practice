c39275d3f9f3c753138c9066fc18f27eb9c88634
oscarg11 <oscar.g2511@gmail.com>
2023-11-27 15:00:49 -0500

    added an alternate solution to looping a triangle

--- JS/eloquentJsExercises.js
+++ JS/eloquentJsExercises.js
@@ -12,8 +12,15 @@ to output the following triangle..
  ****************/
 console.log("1.) Looping A Trianlge")
 
-for(let triangle ="#"; triangle.length < 7; triangle+="#" ){
+for(let triangle ="#"; triangle.length <= 7; triangle+="#" ){
     console.log(triangle);
 }
-
 console.log("--------")
+
+// alternate solution
+console.log("SOLUTION 2")
+let triangle2 = "#"
+while (triangle2.length <= 7){
+    console.log(triangle2)
+    triangle2 += "#"
+}
