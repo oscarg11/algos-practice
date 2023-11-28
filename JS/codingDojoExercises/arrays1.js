7315149192a42a2360f035959472cba12331f378
oscarg11 <oscar.g2511@gmail.com>
2023-11-27 22:54:58 -0500

    completed push front array exercise

---
+++ JS/codingDojoExercises/arrays1.js
@@ -0,0 +1,21 @@
+//1.) Push Front
+/****
+Given an array and an additional value, insert this value at the beginning of the array.
+You may use .push(), you are able to do this without it though!
+Ex:
+pushFront([5,7,2,3], 8) => [8,5,7,2,3]
+pushFront([5,7,2,3]) => [7,99]
+ */
+// function pushFront(arr, val){
+//     arr.push(val)
+//     console.log(arr)
+// }
+// pushFront([1,2,3,4], 5)
+function pushFront(arr, val){
+    for(var i = arr.length; i > 0; i--){
+        arr[i] = arr[i-1];
+    }
+    arr[0] = val;
+    console.log(arr)
+}
+pushFront([1,2,3,4], 5)
