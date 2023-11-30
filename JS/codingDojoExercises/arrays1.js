58680f595c965d9fd9263672d42f438ded02dbd2
oscarg11 <oscar.g2511@gmail.com>
2023-11-30 18:38:52 -0500

    completed Insert At exercise

--- JS/codingDojoExercises/arrays1.js
+++ JS/codingDojoExercises/arrays1.js
@@ -6,7 +6,7 @@ Ex:
 pushFront([5,7,2,3], 8) => [8,5,7,2,3]
 pushFront([5,7,2,3]) => [7,99]
  */
-console.log("Push Front")
+console.log("Push Front:")
 function pushFront(arr, val){
     for(var i = arr.length; i > 0; i--){
         arr[i] = arr[i-1];
@@ -18,7 +18,7 @@ pushFront([1,2,3,4], 5)
 console.log("-------------")
 
 //2.) Pop Front
-console.log("Pop Front")
+console.log("Pop Front:")
 /**
 Given an array, remove and return the value at the beginning of the array.
 Prove the value is removed from the array by printing it. You may use .pop(),
@@ -29,9 +29,9 @@ popFront([0,5,10,15]) => 0 returned, with [5,10,15] printed in the function
 popFront([4,5,7,9]) => 4 returned, with [5,7,9] printed in the function
  */
 function popFront(arr){
-    //save the value at the beginning of the array
-    var removedValue = arr[0];
 
+    var removedValue = arr[0];
+    //shifts indexs to the left
     for(var i = 0; i < arr.length -1; i++){
         arr[i] = arr[i + 1]
     }
@@ -41,3 +41,26 @@ function popFront(arr){
     
 }
 popFront([1,2,3,4])
+console.log("-------------")
+
+//3.) Insert At
+console.log("Insert At:")
+/****** 
+Given an array, index, and additional value, insert the value into array at given index.
+You can think of pushFront(arr,val) as equivalent to insertAt(arr,0,val). 
+You may use .push(), you are able do this without it though!
+Ex:
+insertAt([100,200,5], 2, 311) => [100,200,311,5]
+insertAt([9,33,7], 1, 42) => [9,42,33,7]
+*/
+function insertAt(arr, index, val){
+    for(var i = arr.length; i > index; i--){
+        arr[i] = arr[i-1];
+    }
+    arr[index] = val;
+    console.log(arr);
+}
+insertAt([1,2,3,4],3,45)
+console.log("-------------")
+
+
