8966b6e0484ee96cfa0d70947f03b225b6025ab0
oscarg11 <oscar.g2511@gmail.com>
2023-12-08 19:58:10 -0500

    Completed FizzBuzz exercise

--- JS/eloquentJsExercises.js
+++ JS/eloquentJsExercises.js
@@ -39,9 +39,11 @@ for numbers that are divisible by both 3 and 5 (and still print "Fizz" and "Buzz
 for numbers divisible by only one of those.)
  */
 for(let i = 1;i <= 100; i++){
-    if( i % 3 === 0 ){
+    if( i % 3 === 0 && i % 5 ===0){
+        console.log("FizzBuzz")
+    }else if( i % 3 ===0){
         console.log("Fizz")
-    }else if( i % 5 ===0){
+    }else if (i % 5 ===0 ){
         console.log("Buzz")
     }else{
         console.log(i)
