ddc7b5de3b06ede2b963730d569ba681529597be
oscarg11 <oscar.g2511@gmail.com>
2024-08-02 18:47:40 -0400

    solved popFront array exercise

--- JS/codingDojoExercises/arrays1.js
+++ JS/codingDojoExercises/arrays1.js
@@ -28,7 +28,16 @@ Ex:
 popFront([0,5,10,15]) => 0 returned, with [5,10,15] printed in the function
 popFront([4,5,7,9]) => 4 returned, with [5,7,9] printed in the function
  */
+function popFront(arr){
+    let removedValue = arr[0];
+    for(var i = 0; i < arr.length; i++){
+        arr[i] = arr[i + 1];
+    }
+    arr.length--;
+    console.log("Removed value: ", removedValue)
+    console.log(arr)
 
+}
 popFront([1,2,3,4])
 
 
@@ -43,7 +52,7 @@ insertAt([100,200,5], 2, 311) => [100,200,311,5]
 insertAt([9,33,7], 1, 42) => [9,42,33,7]
 */
 
-insertAt([1,2,3,4],2,99)
+//insertAt([1,2,3,4],2,99)
 
 
 // BONUS: Remove At
@@ -59,7 +68,7 @@ removeAt([1000,3,204,77], 1) => 3 returned, with [1000,204,77] printed in the fu
 removeAt([8,20,55,44,98], 3) => 44 returned, with [8,20,55,98] printed in the function
  */
 
-removeAt([1,2,99,3,4], 2)
+//removeAt([1,2,99,3,4], 2)
 
 
 //BONUS: Swap Pairs
