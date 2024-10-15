87f30c10cea14339951986bcc8f7a7f7290822df
oscarg11 <oscar.g2511@gmail.com>
2024-10-14 23:38:44 -0400

    completed first exercise Minimum

---
+++ JS/eloquentJsExercises/ch3.js
@@ -0,0 +1,16 @@
+/*
+    1.) Minimun
+        Chapter 2 introduced the standard function Math.min, which returns its
+        smallest argument (see "Return Values" on page 27). We can build something
+        like that now. Write a function min that takes two arguments and returns 
+        their minimuns.
+ */
+console.log("1.) Minimum")
+
+function min(a, b){
+    return Math.min(a, b);
+}
+
+console.log(min(100,6));
+
+console.log("--------------")
